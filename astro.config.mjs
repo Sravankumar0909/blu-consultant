3a8871deb4eedef843213f0a2fde5e2524c9e407
Sravan Kumar <sravan090901@gmail.com>
2026-05-02 11:09:08 +0530

    domain url changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE || 'http://localhost:4321',
+  site: process.env.SITE || 'http://www.bluconsultantservices.in',
   base: process.env.BASE || (process.env.CI ? '/blu-consultant' : '/'),
   integrations: [tailwind(), sitemap()],
   output: 'static',
